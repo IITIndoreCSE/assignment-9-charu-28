cf688f71e30fce11e39b494a612ac0f04e3d8ace
charu-28 <mems250005016@iiti.ac.in>
2025-10-17 16:03:42 +0530

    Update Q1.cpp

--- Q1.cpp
+++ Q1.cpp
@@ -1,1 +1,18 @@
+#include<iostream>
+using namespace std;
 
+struct item{
+     int id;
+     float cost;};
+     
+int main(){
+        item items[6]={{1,22.5},{2,15},{3,72.5},{4,55}};
+        
+ cout<<"items with cost greater than 50.00 "<<endl;       
+ for( int i=0;i<6;i++){
+   if(items[i].cost>50.00){
+   cout<<"Item ID:"<<items[i].id<<" Cost:"<<items[i].cost<<endl;}
+   }
+   return 0;
+   }
+ 
